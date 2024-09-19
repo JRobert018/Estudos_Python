6da540e24df85bf08164c5fd59a6647d8f90d4ea
JRobert018 <jbetinho31@gmail.com>
2024-09-19 18:13:11 -0300

    Entendo o que é uma lista

---
+++ S4/listas/teoria/baseListas/oqueELista.py
@@ -0,0 +1,13 @@
+
+#O tipo list em python é uma forma de guarda vários dados na mesma variável,sendo de qualquer tipo,sendo muáveis.
+
+#Temos 2 formas de declarar uma lista em python a primeira sendo:
+
+#        0 1 2 3 4
+#       -5-4-3-2-1
+lista = [1,2,3,4,5]# nome da lista seguido de um cochete que representa o conteudo da lista.
+lista2 = list()#usa o métodos list para a declaração. 
+print(f"informando o índice positivo:{lista[2]}")
+print(f"informando o índice negativo:{lista[-3]}")
+
+
