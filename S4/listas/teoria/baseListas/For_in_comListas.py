5ca69522b99f62e41150cd0ee52c2dcc9fb9c42f
JRobert018 <jbetinho31@gmail.com>
2024-09-19 18:11:40 -0300

    Uso do for em listas

---
+++ S4/listas/teoria/baseListas/For_in_comListas.py
@@ -0,0 +1,7 @@
+# Com o método for podemos ler todos os índices de uma lista.
+lista = ["Roberto","Pedro","Junior","Luiz"]
+i = 0
+#  'nome' vai ser o contéudo da lista.
+for nome in lista:# e 'lista' é a variável que será percorrida 
+   print(f"índice {i}: {nome}")
+   i += 1
